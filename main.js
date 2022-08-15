d9f786207e6e08ebd2d87951ceb13646fb9420c7
Abis <abisali@hotmail.com>
2022-08-15 13:53:08 +0400

    Integrating speech

--- main.js
+++ main.js
@@ -3,5 +3,17 @@ let speak_button = document.querySelector("#speak-button")
 
 let speaknow = () => {
     var speech = new SpeechSynthesisUtterance();
+    speech.rate = 1;
+    speech.pitch = -2;
+    speech.volume = 1;
+    speech.lang = "en-US";
+    speech.voice = speechSynthesis.getVoices()[0];
+    speech.text = text.value;
+    speechSynthesis.speak(speech);
 
-}
+};
+
+speak_button.onclick = function(e) {
+    e.preventDefault();
+    speaknow();
+};
