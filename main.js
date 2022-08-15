e97a1ff2a839ab948f6ec857b8fe026467e27582
Abis <abisali@hotmail.com>
2022-08-15 13:35:19 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let text = document.querySelector("#text");
+let speak_button = document.querySelector("#speak-button")
+
+let speaknow = () => {
+    var speech = new SpeechSynthesisUtterance();
+
+}
