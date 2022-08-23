8ff6416a06b7a1d0e2e8e1276807288153e186fe
Abis <abisali@hotmail.com>
2022-08-23 14:29:45 +0400

    Revamp

---
+++ main.js
@@ -0,0 +1,18 @@
+let text = document.querySelector("#message-828c");
+let speak_button = document.querySelector("#speak-button")
+
+let speaknow = () => {
+    var speech = new SpeechSynthesisUtterance();
+    speech.rate = 1;
+    speech.pitch = -2;
+    speech.volume = 1;
+    speech.lang = "en-US";
+    speech.voice = speechSynthesis.getVoices()[0];
+    speech.text = text.value;
+    speechSynthesis.speak(speech);
+
+};
+
+speak_button.onclick = function(){
+    speaknow();
+};
